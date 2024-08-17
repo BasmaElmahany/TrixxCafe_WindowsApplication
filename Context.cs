65b27c9089e1ca22a671a280894182540bb276a7
Basma <basmakhalaf974@gmail.com>
2024-08-17 20:33:51 +0200

    Creat Project

---
+++ Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trixx_CafeSystem
+{
+    class Context:DbContext
+    {
+
+        public Context() : base(@"Data Source=LOLITA\MSSQLSERVER02;initial catalog=Trixx;integrated security=True;")
+        {
+
+
+        }
+
+
+    }
+}
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Trixx_CafeSystem.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Trixx_CafeSystem.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Trixx_CafeSystem.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
