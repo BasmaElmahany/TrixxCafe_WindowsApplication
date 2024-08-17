6d61d9626c84bb17ada410dd6fc8c6b5e19e16be
AmgaadEzzat <amgaadezzat99@gmail.com>
2024-08-18 00:39:02 +0300

    Creating bill class with

---
+++ Bill.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trixx_CafeSystem
+{
+    internal class Bill
+    {
+        public int Id { get; set; }
+        [ForeignKey("Order")]
+        public int? OrderId { get; set; }
+        public double Price_Of_Product { get; set; }
+        [Required]
+        public string Name_Of_Product { get; set; }
+        public int Product_Qty { get; set; }
+        public double Payment_Amount { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+        public virtual Order Order { get; set; }
+    }
+}
