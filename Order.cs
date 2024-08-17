8ce93de8f0df8396b71f816c8b990f4e6ccc9230
abdelhafez <hafezza08@gmail.com>
2024-08-18 02:51:08 +0300

    add class order

---
+++ Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Trixx_CafeSystem
+{
+   public class Order
+    {
+        [Key]
+        public int Order_Id { get; set; }
+        [ForeignKey("Login_User")]
+        public int User_Id { get; set; }
+
+        public Login_User Login_User { get; set; }
+        public DateTime Date { get; set; }
+        public double Total_Amount { get; set; }
+
+       
+        public DateTime Time { get; set; }
+
+    }
+}
