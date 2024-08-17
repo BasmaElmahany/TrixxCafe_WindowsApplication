c65bec27e4e034af021bb456a9fe0c38dd3e7fb3
Karimashour <https://github.com/Karimashour>
2024-08-18 01:13:23 +0300

    adding class products

---
+++ Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trixx_CafeSystem
+{
+    public class Product
+    {
+        [Key]
+        public int Product_ID { get; set; }
+
+        [ForeignKey("Login_User")]
+        public int User_ID { get; set; }
+        public Login_User Login_User { get; set; }
+
+        [ForeignKey("Category")]
+        public int Category_ID { get; set; }
+        public virtual Category Category { get; set; }
+
+        public int Stock_Qty { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+    }
+}
