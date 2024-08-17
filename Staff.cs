614880c574f79d645290744c630e4f7f890a1eed
Aly Abo Zaid <ali.abozaidazz@gmail.com>
2024-08-18 00:45:09 +0300

    add table staff

---
+++ Staff.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trixx_CafeSystem
+{
+    internal class Staff
+    {
+        public int Staff_ID { get; set; }
+        public string Staff_Name { get; set; }
+        public int User_ID {  get; set; }
+        public int NID { get; set; }
+        public int Address { get; set; }
+        public DateTime Assign_Date { get; set; }
+        public string Staff_Phone { get; set; }
+        public virtual Login_User Login_User { get; set; }
+
+        // Don't forget to name the table => Salaries when you create it  
+        public virtual Salaries Salary { get; set; } // one to one relationship
+
+    }
+}
