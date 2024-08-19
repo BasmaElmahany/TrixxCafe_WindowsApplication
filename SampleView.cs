333a49f531b8d3c492501e831c3d2b524308c257
ElhamSafwat/Elham <elham5972@gmail.com>
2024-08-19 13:10:32 +0300

    add SampleView Form

---
+++ SampleView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trixx_CafeSystem
+{
+    public partial class SampleView : Form
+    {
+        public SampleView()
+        {
+            InitializeComponent();
+        }
+
+        public virtual void AddBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public virtual void SearchTxtBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
