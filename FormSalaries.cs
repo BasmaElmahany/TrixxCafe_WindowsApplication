2ad52e744b56a089616463088bdc3761f1a00165
Basma <basmakhalaf974@gmail.com>
2024-08-19 13:26:07 +0200

    FormSalaries

---
+++ FormSalaries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trixx_CafeSystem
+{
+    public partial class FormSalaries : SampleViewForSalaries
+    {
+        public FormSalaries()
+        {
+            InitializeComponent();
+        }
+        public override void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public override void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
