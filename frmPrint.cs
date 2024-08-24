f661dd2e24a7edd122115efd2609549ede722b6b
AmgaadEzzat <amgaadezzat99@gmail.com>
2024-08-24 14:22:59 +0300

    creating frmPrint Form to show inside it the reports

---
+++ frmPrint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trixx_CafeSystem
+{
+    public partial class frmPrint : Form
+    {
+        public frmPrint()
+        {
+            InitializeComponent();
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+            btnMax.PerformClick();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
