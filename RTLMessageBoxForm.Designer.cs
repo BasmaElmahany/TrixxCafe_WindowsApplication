54a9b64e2701e39da3e8fccd330b27736858cf4d
AmgaadEzzat <amgaadezzat99@gmail.com>
2024-08-24 14:18:56 +0300

    creating custom message box

---
+++ RTLMessageBoxForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Trixx_CafeSystem
+{
+    partial class RTLMessageBoxForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnYes = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(97, 30);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(53, 20);
+            this.lblMessage.TabIndex = 0;
+            this.lblMessage.Text = "label1";
+            // 
+            // btnYes
+            // 
+            this.btnYes.DialogResult = System.Windows.Forms.DialogResult.Yes;
+            this.btnYes.Location = new System.Drawing.Point(144, 89);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(78, 33);
+            this.btnYes.TabIndex = 1;
+            this.btnYes.Text = "Yes";
+            this.btnYes.UseVisualStyleBackColor = true;
+            // 
+            // btnNo
+            // 
+            this.btnNo.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.btnNo.Location = new System.Drawing.Point(36, 89);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(78, 33);
+            this.btnNo.TabIndex = 2;
+            this.btnNo.Text = "No";
+            this.btnNo.UseVisualStyleBackColor = true;
+            // 
+            // RTLMessageBoxForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 146);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnYes);
+            this.Controls.Add(this.lblMessage);
+            this.Name = "RTLMessageBoxForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RTLMessageBoxForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.Button btnNo;
+    }
+}
---
+++ RTLMessageBoxForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Services.Description;
+using System.Windows.Forms;
+
+namespace Trixx_CafeSystem
+{
+    public partial class RTLMessageBoxForm : Form
+    {
+        public RTLMessageBoxForm(string message, string title)
+        {
+            InitializeComponent();
+            this.Text = title;
+
+            // Set the message text and adjust properties for RTL support
+            lblMessage.Text = message;
+            lblMessage.RightToLeft = RightToLeft.Yes;
+            lblMessage.TextAlign = ContentAlignment.MiddleRight;
+
+            // Set the buttons' text and adjust properties for RTL support
+            btnYes.Text = "نعم";
+            btnNo.Text = "لا";
+            btnYes.RightToLeft = RightToLeft.Yes;
+            btnNo.RightToLeft = RightToLeft.Yes;
+        }
+
+        private void btnYes_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Yes;
+            this.Close();
+        }
+
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.No;
+            this.Close();
+        }
+
+        public static DialogResult Show(string message, string title)
+        {
+            using (var form = new RTLMessageBoxForm(message, title))
+            {
+                return form.ShowDialog();
+            }
+        }
+    }
+}
